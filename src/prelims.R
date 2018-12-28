86ec8d5143f28e0c7c13116a7b6dfd8dcbb2c991
Stephen Simon <mail@pmean.com>
2018-12-28 15:54:56 -0600

    Fixed smart quotes and dashes.

---
+++ src/prelims.R
@@ -0,0 +1,19 @@
+suppressMessages(suppressWarnings(library(lubridate)))
+suppressMessages(suppressWarnings(library(magrittr)))
+dat1 <- as.Date("2019-01-21", format="%Y-%m-%d")
+d <- dat1+(0:15)*7
+f <- "%A, %B %d, %Y"
+mon <- format(d, f)
+tue <- format(d+1, f)
+wed <- format(d+2, f)
+thu <- format(d+3, f)
+fri <- format(d+4, f)
+read_text <- function(fn, due="") {
+  "../data/" %>%
+    paste0(fn) %>% 
+    paste0(".txt") %>%
+    readLines %>% 
+    gsub("<<due date>>", due, .) %>% 
+    paste0(collapse="\n") %>%
+    return
+}
